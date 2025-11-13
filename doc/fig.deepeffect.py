591b742f8d60fce39c6ef42d7282d6ed84f59b05
chinesebear <swubear@163.com>
2025-11-13 14:36:57 +0000

    ablation figure

--- doc/fig.deepeffect.py
+++ doc/fig.deepeffect.py
@@ -7,87 +7,162 @@ plt.rc('font',family='Times New Roman')
 
 
 # 创建数据
-methods = ['DFEM', 'DFEM + MVFCLN']
+methods = ['Basic','+FESNet','+SRNet', '+CVAM', '+CLM']
 categories = ['MFO', 'BPO', 'CCO']
 
 # Fmax数据
-fmax_data = {
-    'DFEM': [0.536, 0.496, 0.622],
-    'DFEM + MVFCLN': [0.547, 0.517, 0.631]
-}
+fmax_data = [
+    [0.512, 0.478, 0.601],
+    [0.536, 0.496, 0.622],
+    [0.541, 0.503, 0.626],
+    [0.545, 0.511, 0.628],
+    [0.547, 0.517, 0.631]
+]
+fmax_data = np.array(fmax_data)
+  
 
 # Smin数据
-smin_data = {
-    'DFEM': [8.366, 22.376, 7.735],
-    'DFEM + MVFCLN': [8.274, 21.683, 7.704]
-}
+smin_data = [
+    [9.123, 23.456, 8.234], 
+    [8.789, 22.987, 7.945],
+    [8.512, 22.543, 7.856],
+    [8.423, 22.123, 7.789],
+    [8.274, 21.683, 7.704]
+]
+smin_data = np.array(smin_data)
 
 # AUPR数据
-aupr_data = {
-    'DFEM': [0.495, 0.462, 0.626],
-    'DFEM + MVFCLN': [0.508, 0.469, 0.634]
-}
-
+aupr_data = [
+    [0.482, 0.451, 0.612],
+    [0.501, 0.463, 0.623],
+    [0.506, 0.467, 0.628],
+    [0.506, 0.467, 0.630],
+    [0.508, 0.469, 0.634]
+]
+aupr_data = np.array(aupr_data)
 # 创建图形和子图
-plt.rcParams.update({'font.size': 18})
-fig, axes = plt.subplots(1, 3, figsize=(12, 4))
+plt.rcParams.update({'font.size': 16})
+fig, axes = plt.subplots(3, 3, figsize=(12, 12))
 
 # 设置颜色
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c']
-bar_width = 0.35
-x = np.arange(len(categories))
+# colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
+patterns = ['/', '\\', 'x', '/', '\\']
+# patterns = ['/', '\\', 'x', 'o', '.']
+colors = ['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f']
+bar_width = 0.7
+x = np.arange(len(methods))
 
 # 绘制Fmax子图
-for i, method in enumerate(methods):
-    offset = bar_width * i
-    axes[0].bar(x + offset, fmax_data[method], bar_width,
-                label=method, color=colors[i], alpha=0.8)
+bars = axes[0,0].bar(x,fmax_data[:,0], bar_width, color=colors,edgecolor='black', alpha=0.8)
+axes[0,0].bar_label(bars, labels=[f'{v:.3f}' for v in fmax_data[:,0]], padding=3)
+axes[0,0].set_xlabel('(a)', fontweight='bold')
+axes[0,0].set_ylabel('Fmax', fontweight='bold')
+axes[0,0].set_ylim(0.5, 0.58)
+axes[0,0].set_xticks(x)
+axes[0,0].set_xticklabels(methods, rotation=25)
+axes[0,0].set_title(categories[0], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
 
-axes[0].set_xlabel('a', fontweight='bold')
-axes[0].set_ylabel('Fmax', fontweight='bold')
-# axes[0].set_title('Fmax', fontweight='bold')
-axes[0].set_xticks(x + bar_width / 2)
-axes[0].set_xticklabels(categories)
-# axes[0].legend()
-axes[0].set_ylim(0.45, 0.65)
-axes[0].set_yticks(np.arange(0.45, 0.65, 0.03))
-axes[0].grid()
+bars = axes[0,1].bar(x,fmax_data[:,1], bar_width, color=colors, alpha=0.8)
+axes[0,1].bar_label(bars, labels=[f'{v:.3f}' for v in fmax_data[:,1]], padding=3)
+axes[0,1].set_xlabel('(b)', fontweight='bold')
+axes[0,1].set_ylabel('Fmax', fontweight='bold')
+axes[0,1].set_ylim(0.470, 0.550)
+axes[0,1].set_xticks(x)
+axes[0,1].set_xticklabels(methods, rotation=25)
+axes[0,1].set_title(categories[1], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
+
+bars = axes[0,2].bar(x,fmax_data[:,2], bar_width, color=colors, alpha=0.8)
+axes[0,2].bar_label(bars, labels=[f'{v:.3f}' for v in fmax_data[:,2]], padding=3)
+axes[0,2].set_xlabel('(c)', fontweight='bold')
+axes[0,2].set_ylabel('Fmax', fontweight='bold')
+axes[0,2].set_ylim(0.6, 0.640)
+axes[0,2].set_xticks(x)
+axes[0,2].set_xticklabels(methods, rotation=25)
+axes[0,2].set_title(categories[2], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
 
 # 绘制Smin子图
-for i, method in enumerate(methods):
-    offset = bar_width * i
-    axes[1].bar(x + offset, smin_data[method], bar_width,
-                label=method, color=colors[i], alpha=0.8)
+bars = axes[1,0].bar(x,smin_data[:,0], bar_width, color=colors, alpha=0.8)
+axes[1,0].bar_label(bars, labels=[f'{v:.3f}' for v in smin_data[:,0]], padding=3)
+axes[1,0].set_xlabel('(d)', fontweight='bold')
+axes[1,0].set_ylabel('Smin', fontweight='bold')
+axes[1,0].set_ylim(8, 9.5)
+axes[1,0].set_xticks(x)
+axes[1,0].set_xticklabels(methods, rotation=25)
+axes[1,0].set_title(categories[0], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
 
-axes[1].set_xlabel('b', fontweight='bold')
-axes[1].set_ylabel('Smin', fontweight='bold')
-# axes[1].set_title('Smin', fontweight='bold')
-axes[1].set_xticks(x + bar_width / 2)
-axes[1].set_xticklabels(categories)
-# axes[1].legend()
-# axes[1].set_yticks(np.arange(0, 23, 2))
-# axes[1].set_ylim(0, 40)
-axes[1].grid()
+bars = axes[1,1].bar(x,smin_data[:,1], bar_width, color=colors, alpha=0.8)
+axes[1,1].bar_label(bars, labels=[f'{v:.3f}' for v in smin_data[:,1]], padding=3)
+axes[1,1].set_xlabel('(e)', fontweight='bold')
+axes[1,1].set_ylabel('Smin', fontweight='bold')
+axes[1,1].set_ylim(21, 24)
+axes[1,1].set_xticks(x)
+axes[1,1].set_xticklabels(methods, rotation=25)
+axes[1,1].set_title(categories[1], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
+
+bars = axes[1,2].bar(x,smin_data[:,2], bar_width, color=colors, alpha=0.8)
+axes[1,2].bar_label(bars, labels=[f'{v:.3f}' for v in smin_data[:,2]], padding=3)
+axes[1,2].set_xlabel('(f)', fontweight='bold')
+axes[1,2].set_ylabel('Smin', fontweight='bold')
+axes[1,2].set_ylim(7.5, 8.5)
+axes[1,2].set_xticks(x)
+axes[1,2].set_xticklabels(methods, rotation=25)
+axes[1,2].set_title(categories[2], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
 
 # 绘制AUPR子图
-for i, method in enumerate(methods):
-    offset = bar_width * i
-    axes[2].bar(x + offset, aupr_data[method], bar_width,
-                label=method, color=colors[i], alpha=0.8)
+bars = axes[2,0].bar(x,fmax_data[:,0], bar_width, color=colors, alpha=0.8)
+axes[2,0].bar_label(bars, labels=[f'{v:.3f}' for v in fmax_data[:,0]], padding=3)
+axes[2,0].set_xlabel('(g)', fontweight='bold')
+axes[2,0].set_ylabel('AUPR', fontweight='bold')
+axes[2,0].set_ylim(0.5, 0.58)
+axes[2,0].set_xticks(x)
+axes[2,0].set_xticklabels(methods, rotation=25)
+axes[2,0].set_title(categories[0], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
 
-axes[2].set_xlabel('c', fontweight='bold')
-axes[2].set_ylabel('AUPR', fontweight='bold')
-# axes[2].set_title('AUPR', fontweight='bold')
-axes[2].set_xticks(x + bar_width / 2)
-axes[2].set_xticklabels(categories)
-# axes[2].legend()
-axes[2].set_ylim(0.45, 0.65)
-axes[2].set_yticks(np.arange(0.45, 0.65, 0.03))
-axes[2].grid()
+bars = axes[2,1].bar(x,fmax_data[:,1], bar_width, color=colors, alpha=0.8)
+axes[2,1].bar_label(bars, labels=[f'{v:.3f}' for v in fmax_data[:,1]], padding=3)
+axes[2,1].set_xlabel('(h)', fontweight='bold')
+axes[2,1].set_ylabel('AUPR', fontweight='bold')
+axes[2,1].set_ylim(0.470, 0.550)
+axes[2,1].set_xticks(x)
+axes[2,1].set_xticklabels(methods, rotation=25)
+axes[2,1].set_title(categories[1], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
 
-# 图例共用
-lines, labels = fig.axes[0].get_legend_handles_labels()
-fig.legend(lines, labels, loc = 'upper center', ncols=2, bbox_to_anchor=(0.5,1.1)) # 图例的位置，bbox_to_anchor=(0.5, 0.92),
+bars = axes[2,2].bar(x,fmax_data[:,2], bar_width, color=colors, alpha=0.8)
+axes[2,2].bar_label(bars, labels=[f'{v:.3f}' for v in fmax_data[:,2]], padding=3)
+axes[2,2].set_xlabel('(i)', fontweight='bold')
+axes[2,2].set_ylabel('AUPR', fontweight='bold')
+axes[2,2].set_ylim(0.6, 0.640)
+axes[2,2].set_xticks(x)
+axes[2,2].set_xticklabels(methods, rotation=25)
+axes[2,2].set_title(categories[2], fontweight='bold')
+# 为每个柱添加不同的纹理
+for bar, pattern in zip(bars, patterns):
+    bar.set_hatch(pattern)
+
 
 # 调整布局
 plt.tight_layout()
